70f6c9e50b13f700343088c4d72d2d98cc6b075b
infor@rentoproperties.co.ke <infor@rentoproperties.co.ke>
2021-09-01 23:30:00 -0700

    stable

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       </transition>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-   <i class="fa fa-ellipsis-v text_color" aria-hidden="true"></i>n
+   <i class="fa fa-ellipsis-v text_color" aria-hidden="true"></i>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
---
+++ src/components/tv_shows.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="parent_div">
+<div class="discoveries">
+
+
+
+</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.discoveries{
+    text-align: left;
+    width: auto;
+    padding: 5px;
+    border-radius: 50px;
+    margin-right: 0px;
+    display: inline-block;
+}
+</style>
